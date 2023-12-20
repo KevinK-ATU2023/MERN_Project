d67dadc86a5a030e52e30a4abe86d6f1ec02a7ad
KevinK-ATU2023 <kkarundu8@gmail.com>
2023-12-20 03:35:18 +0000

    added routing functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// my components
+import Home from './components/home';
+
 function App() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <div>
+        <BrowserRouter>
+          <Routes>
+            {/* Home Page */}
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </>
   );
 }
 
