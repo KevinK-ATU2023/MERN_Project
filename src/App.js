83fb4fdf7e75db4f736d1890df822530a1eb3d8a
KevinK-ATU2023 <kkarundu8@gmail.com>
2023-12-20 12:34:07 +0000

    added client navigation bar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 // my components
 import Home from './components/home';
+import Navigation from './components/navigation';
 
 function App() {
   return (
-    <>
-      <div>
-        <BrowserRouter>
-          <Routes>
-            {/* Home Page */}
-            <Route path="/" element={<Home />} />
-          </Routes>
-        </BrowserRouter>
-      </div>
-    </>
+      <BrowserRouter>
+        {/* Navigation Bar */}
+        <Navigation />
+        <Routes>
+          {/* Home Page */}
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
   );
 }
 
